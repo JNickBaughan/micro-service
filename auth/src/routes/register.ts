308734e220199c86a7cf8e3b9d7c3d40e3a9f1d2
JNickBaughan <JNickBaughan@gmail.com>
2021-08-09 08:29:57 -0400

    add validation to register route

--- auth/src/routes/register.ts
+++ auth/src/routes/register.ts
@@ -1,8 +1,26 @@
 import { Router, Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 
-//todo: make POST route
+const emailValidator = body("email")
+  .isEmail()
+  .withMessage("A valid email is required");
+const pwValidator = body("password")
+  .isLength({ min: 8 })
+  .withMessage("Password must be at least 8 chars long")
+  .isLength({ max: 20 })
+  .withMessage("Password must be no more than 20 chars long");
+
 export const getRegisterRoute = (router: Router) => {
-  router.get("/users/register", function (req: Request, res: Response) {
-    res.send("register handler");
-  });
+  router.post(
+    "/users/register",
+    [emailValidator, pwValidator],
+    function (req: Request, res: Response) {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const { email, password } = req.body;
+      res.send(`user registed with pw of ${email}`);
+    }
+  );
 };
